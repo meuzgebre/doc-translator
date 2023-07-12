3786f2fd349ed469343f51aa0307efe17fcd92a5
meuzgebre <95613515+meuzgebre@users.noreply.github.com>
2023-07-12 12:43:08 +0300

    Implementation for file_parser

--- src/doctranslator/utils/file_parser.py
+++ src/doctranslator/utils/file_parser.py
@@ -1,1 +1,7 @@
+def get_file_name_with_language_extension(file_path, target_language):
+    file_name = file_path.split("/")[-1]
+    file_name_parts = file_name.split(".")
+    base_name = ".".join(file_name_parts[:-1])
+    extension = file_name_parts[-1]
 
+    return f"{base_name}_translated_{target_language}.{extension}"
