92b06764436e7975e8de77e10fbdfae008b7ff01
meuzgebre <95613515+meuzgebre@users.noreply.github.com>
2023-07-13 17:22:28 +0300

    Add error handling

--- src/doctranslator/utils/file_parser.py
+++ src/doctranslator/utils/file_parser.py
@@ -1,7 +1,11 @@
 def get_file_name_with_language_extension(file_path, target_language):
-    file_name = file_path.split("/")[-1]
-    file_name_parts = file_name.split(".")
-    base_name = ".".join(file_name_parts[:-1])
-    extension = file_name_parts[-1]
+    try:
+        file_name = file_path.split("/")[-1]
+        file_name_parts = file_name.split(".")
+        base_name = ".".join(file_name_parts[:-1])
+        extension = file_name_parts[-1]
 
-    return f"{base_name}_translated_{target_language}.{extension}"
+        return f"{base_name}_translated_{target_language}.{extension}"
+    except Exception as e:
+        print(f"Error occurred: {e}")
+        return None
